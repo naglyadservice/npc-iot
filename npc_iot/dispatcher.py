573590b689b2cbf0392c4785c846d92d911f599c
toxazhl <zhltoxa@gmail.com>
2025-02-26 11:49:01 +0200

    improve dispatcher

--- npc_iot/dispatcher.py
+++ npc_iot/dispatcher.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import re
 from contextlib import AsyncExitStack, asynccontextmanager
+from functools import partial
 from typing import Any, AsyncIterator, Callable, Coroutine
 
 from .connectors.base import BaseConnector
@@ -18,17 +19,10 @@ def _extract_device_id(topic_prefix: str, topic: str) -> str:
 
 
 class MessageHandler:
-    def __init__(
-        self,
-        topic_prefix: str,
-        topic: str,
-        payload_decoder: Callable[[str | bytes], Any],
-        callback_kwargs: dict[str, Any] | None = None,
-    ) -> None:
-        self._topic_prefix = topic_prefix
+    def __init__(self, topic: str, is_ack: bool = False, is_result: bool = False) -> None:
         self.topic = topic
-        self._payload_decoder = payload_decoder
-        self._callback_kwargs = callback_kwargs or {}
+        self.is_ack = is_ack
+        self.is_result = is_result
         self._callbacks: list[Callable[[str, dict[str, Any]], Coroutine]] = []
 
     def register_callback(self, callback: Callable[[str, dict[str, Any]], Coroutine]) -> None:
@@ -42,70 +36,99 @@ class MessageHandler:
 
         raise ValueError("Callback not found")
 
-    async def _handle_message(self, topic: str, payload: str | bytes) -> None:
-        decoded_payload = self._payload_decoder(payload)
-        device_id = _extract_device_id(self._topic_prefix, topic)
+    async def _handle_message(
+        self,
+        topic: str,
+        payload: str | bytes,
+        payload_decoder: Callable[[str | bytes], Any],
+        topic_prefix: str,
+        callback_kwargs: dict[str, Any],
+    ) -> None:
+        decoded_payload = payload_decoder(payload)
+        device_id = _extract_device_id(topic_prefix, topic)
         asyncio.gather(
             *[
-                callback(device_id, decoded_payload, **self._callback_kwargs)
+                callback(device_id, decoded_payload, **callback_kwargs)
                 for callback in self._callbacks
             ]
         )
 
     @asynccontextmanager
-    async def handle_messages(self, connector: BaseConnector) -> AsyncIterator[None]:
-        async with connector.subscribe(topic=self.topic, callback=self._handle_message):
+    async def handle_messages(
+        self,
+        connector: BaseConnector,
+        topic_prefix: str,
+        payload_decoder: Callable[[str | bytes], Any],
+        callback_kwargs: dict[str, Any] | None = None,
+    ) -> AsyncIterator[None]:
+        callback = partial(
+            self._handle_message,
+            payload_decoder=payload_decoder,
+            topic_prefix=topic_prefix,
+            callback_kwargs=callback_kwargs or {},
+        )
+
+        async with connector.subscribe(topic=f"{topic_prefix}{self.topic}", callback=callback):
             yield
 
 
 class Dispatcher:
+    begin = MessageHandler(topic="/+/server/begin")
+    reboot_ack = MessageHandler(topic="/+/server/reboot/ack", is_ack=True)
+    config_ack = MessageHandler(topic="/+/server/config/ack", is_ack=True)
+    config = MessageHandler(topic="/+/server/config")
+    setting_ack = MessageHandler(topic="/+/server/setting/ack", is_ack=True)
+    setting = MessageHandler(topic="/+/server/server/setting")
+    state_ack = MessageHandler(topic="/+/server/state/ack", is_ack=True)
+    state = MessageHandler(topic="/+/server/state", is_result=True)
+    state_info = MessageHandler(topic="/+/server/state/info")
+
+    _callback_handlers = [
+        begin,
+        reboot_ack,
+        config_ack,
+        config,
+        setting_ack,
+        setting,
+        state_ack,
+        state,
+        state_info,
+    ]
+
     def __init__(
         self,
         topic_prefix: str,
         payload_decoder: Callable[[str | bytes], Any],
         callback_kwargs: dict[str, Any] | None = None,
     ) -> None:
+        self._topic_prefix = topic_prefix
         self._payload_decoder = payload_decoder
-        kwargs = {
+        self._callback_kwargs = callback_kwargs
+
+        self._handler_kwargs = {
+            "topic_prefix": topic_prefix,
             "payload_decoder": payload_decoder,
             "callback_kwargs": callback_kwargs,
         }
-
-        self.begin = MessageHandler(f"{topic_prefix}/+/server/begin", **kwargs)
-        self.reboot_ack = MessageHandler(f"{topic_prefix}/+/server/reboot/ack", **kwargs)
-        self.config_ack = MessageHandler(f"{topic_prefix}/+/server/config/ack", **kwargs)
-        self.config = MessageHandler(f"{topic_prefix}/+/server/config", **kwargs)
-        self.setting_ack = MessageHandler(f"{topic_prefix}/+/server/setting/ack", **kwargs)
-        self.setting = MessageHandler(f"{topic_prefix}/+/server/setting", **kwargs)
-        self.state_ack = MessageHandler(f"{topic_prefix}/+/server/state/ack", **kwargs)
-        self.state = MessageHandler(f"{topic_prefix}/+/server/state", **kwargs)
-        self.state_info = MessageHandler(f"{topic_prefix}/+/server/state/info", **kwargs)
-
-        self._callback_handlers = [
-            self.begin,
-            self.reboot_ack,
-            self.config_ack,
-            self.config,
-            self.setting_ack,
-            self.setting,
-            self.state_ack,
-            self.state,
-            self.state_info,
-        ]
 
     @asynccontextmanager
     async def start_handling(self, connector: BaseConnector) -> AsyncIterator[None]:
         async with AsyncExitStack() as exit_stack:
             for callback_handler in self._callback_handlers:
-                await exit_stack.enter_async_context(callback_handler.handle_messages(connector))
+                await exit_stack.enter_async_context(
+                    callback_handler.handle_messages(
+                        connector,
+                        topic_prefix=self._topic_prefix,
+                        payload_decoder=self._payload_decoder,
+                        callback_kwargs=self._callback_kwargs,
+                    )
+                )
 
             yield
 
     def register_callbacks(
         self, result_callback: Callable[[str, dict[str, Any]], Coroutine]
     ) -> None:
-        self.reboot_ack.register_callback(result_callback)
-        self.state_ack.register_callback(result_callback)
-        self.config_ack.register_callback(result_callback)
-        self.setting_ack.register_callback(result_callback)
-        self.state.register_callback(result_callback)
+        for callback_handler in self._callback_handlers:
+            if callback_handler.is_ack or callback_handler.is_result:
+                callback_handler.register_callback(result_callback)

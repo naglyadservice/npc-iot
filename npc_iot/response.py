907d843eaa47969f7a3d53fc338e4566b13a34fd
toxazhl <zhltoxa@gmail.com>
2025-02-26 13:37:18 +0200

    fix ResponseWaiter

--- npc_iot/response.py
+++ npc_iot/response.py
@@ -1,28 +1,29 @@
 import asyncio
 import logging
-from typing import Any
+from typing import Any, TypeVar
+
+from .types import BaseResponse
 
 log = logging.getLogger(__name__)
 
 
-class ResponseWaiter:
+ResponseWaiterType = TypeVar("ResponseWaiterType", bound=BaseResponse)
+
+
+class ResponseWaiter[ResponseWaiterType]:
     def __init__(self, device_id: str, request_id: int, ttl: int | None) -> None:
         self.device_id = device_id
         self.request_id = request_id
         self.ttl = ttl
-        self._future = asyncio.Future[dict[str, Any]]()
+        self._future = asyncio.Future()
 
-    def set_result(self, result: dict[str, Any]) -> None:
+    def _set_result(self, result: dict[str, Any]) -> None:
         self._future.set_result(result)
 
-    def set_exception(self, exception: Exception) -> None:
+    def _set_exception(self, exception: Exception) -> None:
         self._future.set_exception(exception)
 
-    async def wait(self, timeout: float | None = 60) -> dict[str, Any]:
+    async def wait(self, timeout: float | None = 60) -> ResponseWaiterType:
         if timeout is None:
             return await self._future
-        try:
-            return await asyncio.wait_for(self._future, timeout)
-        except asyncio.TimeoutError:
-            self._future.cancel("timeout")
-            raise
+        return await asyncio.wait_for(self._future, timeout)
